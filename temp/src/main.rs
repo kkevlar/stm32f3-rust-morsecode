bca5828da54ceaef861ceef1bb485da177556db8
Kevin Kellar <kellar.kevin@gmail.com>
2021-02-14 13:08:22 -0800

    tenatively teh stack size oto big

--- temp/src/main.rs
+++ temp/src/main.rs
@@ -423,15 +423,16 @@ fn main() -> ! {
     // stuff::poll_morse();
     poll_morse(0, tim6, gpioa, 10);
 
-    let ms = 50;
-    loop {
-        for curr in 0..8 {
-            let next = (curr + 1) % 8;
+    // let ms = 50;
+    // loop {
+    //     for curr in 0..8 {
+    //         let next = (curr + 1) % 8;
 
-            leds[next].on();
-            delay(tim6, ms);
-            leds[curr].off();
-            delay(tim6, ms);
-        }
-    }
+    //         leds[next].on();
+    //         delay(tim6, ms);
+    //         leds[curr].off();
+    //         delay(tim6, ms);
+    //     }
+    // }
+    loop {}
 }
